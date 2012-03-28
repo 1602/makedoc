9d7cbe8b1ba86d9e622390b29041d2180b4d9c76
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.com>
2012-03-29 01:23:26 +0400

    Git links, non-classes files, submenus

--- bin/makedoc.js
+++ bin/makedoc.js
@@ -4,13 +4,22 @@ var argv = require('optimist')
     .usage('Generate bootstrapped jsdoc.\nUsage: $0')
     .alias('d', 'dir')
     .describe('d', 'Load all *.js files inside directory')
+
     .alias('t', 'title')
     .describe('t', 'Specify common title of documentation pages')
+
+    .alias('g', 'git')
+    .describe('g', 'Github repo (add fancy links to sources)')
+
     .argv
 
 var title = argv.t;
+var git = argv.g;
 
 argv._.forEach(function (file) {
-    require('../lib/doc').generateFile(file, title);
+    require('../lib/doc').generateFile(file, {
+        title: title,
+        git: git
+    });
 });
 
