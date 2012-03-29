d62594b02db05fd645e1ae5e785f97568584d319
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.com>
2012-03-29 17:59:11 +0400

    github downloader

--- bin/makedoc.js
+++ bin/makedoc.js
@@ -2,8 +2,6 @@
 
 var argv = require('optimist')
     .usage('Generate bootstrapped jsdoc.\nUsage: $0')
-    .alias('d', 'dir')
-    .describe('d', 'Load all *.js files inside directory')
 
     .alias('t', 'title')
     .describe('t', 'Specify common title of documentation pages')
@@ -11,15 +9,26 @@ var argv = require('optimist')
     .alias('g', 'git')
     .describe('g', 'Github repo (add fancy links to sources)')
 
+    .alias('d', 'download')
+    .describe('d', 'Download files from github public repo')
+
     .argv
 
 var title = argv.t;
 var git = argv.g;
 
-argv._.forEach(function (file) {
+if (argv.d && git) {
+    require('../lib/downloader').download('/' + git + '/tree/master/' + argv.d, function (files) {
+        files.forEach(generate);
+    });
+} else {
+    argv._.forEach(generate);
+}
+
+function generate(file) {
     require('../lib/doc').generateFile(file, {
         title: title,
         git: git
     });
-});
+}
 
